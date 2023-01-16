1ef736647e1160d02807eb64784c643e5f2ea12b
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-01-16 17:33:40 +0200

    add addEventListener

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://restcountries.com/v3.1/name';
 function fetchCountries(name) {
   return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`).then(response => {
     if (!response.ok) {
-      throw new Error(response.statusText);
+      throw new Error('Oops, there is no country with that name');
     }
     return response.json();
   });
--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import './css/styles.css';
-import './fetchCountries';
+import API from './fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+const countryInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+countryInput.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(event) {
+    event.preventDefault();
+    const entryCountry = e.target.value.trim();
+    console.dir(entryCountry);
+
+}
